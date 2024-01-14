3275b89c1042c9d0e4593510b283eea0c6fe5c16
arthur <arthur.queiroz08@aluno.ifce.edu.br>
2024-01-14 14:43:07 -0300

    feat: form part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,9 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatChipsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
